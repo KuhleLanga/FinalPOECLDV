df5866721bf066c191d98f6ab113aac3a46f8ad0
KuhleLanga <131441505+KuhleLanga@users.noreply.github.com>
2024-06-23 16:18:29 +0200

    FirstSave

---
+++ POEPart2/Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using POEPart2.Models;
+using System.Collections.Generic;
+
+namespace POEPartOne.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Products> products { get; set; }
+       
+    }
+}
---
+++ POEPart2/Models/Products.cs
@@ -0,0 +1,13 @@
+﻿namespace POEPart2.Models
+{
+    public class Products
+    {
+        public int ProductID { get; set; }
+        public string ProductName { get; set; }
+        public string ProductDescription { get; set; }
+        public decimal Price { get; set; }
+        public string Category { get; set; }
+        public bool Availability { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
